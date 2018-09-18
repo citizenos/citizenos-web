da0a26b003f174fd50746e5e6e6a49c7e71e1fbb
Argo Roots <argo@roots.ee>
2018-09-18 12:34:47 +0300

    Remove bottom margin

--- source/_scripts/page-block.js
+++ source/_scripts/page-block.js
@@ -1,6 +1,6 @@
 $(function () {
     $(window).resize(function () {
-        $('.page-block').css('min-height', $(window).height() - $('nav').outerHeight(true) - 30 + 'px')
+        $('.page-block').css('min-height', $(window).height() - $('nav').outerHeight(true) + 'px')
     })
 
     $(window).trigger('resize')
