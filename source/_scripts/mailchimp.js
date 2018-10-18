654be9805f8fdf1d57f76be5574d8f10b6285a88
Argo Roots <argo@roots.ee>
2018-10-18 14:08:46 +0300

    Move Mailchimp script to separate JS

---
+++ source/_scripts/mailchimp.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('a[href="#subscribe"]').click(function () {
+        window.dojoRequire(['mojo/signup-forms/Loader'], function(L) {
+            L.start({
+                baseUrl: 'mc.us19.list-manage.com',
+                uuid: 'ab576406496d96d9a8387879f',
+                lid: 'd44f990ec5',
+                uniqueMethods: true
+            })
+
+            document.cookie = 'MCPopupClosed=; expires=Thu, 01 Jan 1970 00:00:00 UTC'
+            document.cookie = 'MCPopupSubscribed=; expires=Thu, 01 Jan 1970 00:00:00 UTC'
+        })
+    })
+})
