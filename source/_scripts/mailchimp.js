6065cd531bd8e309d042128478dac74947e23aeb
Argo Roots <argo@roots.ee>
2019-05-14 16:24:03 +0300

    Rename newsletter popup

--- source/_scripts/mailchimp.js
+++ source/_scripts/mailchimp.js
@@ -1,29 +1,29 @@
 $(function () {
-    $('#mailchimp-form').on('submit', function(e) {
+    $('#newsletter-form').on('submit', function(e) {
         e.preventDefault()
 
-        var email = $('#mailchimp-form input[name="EMAIL"]').val()
+        var email = $('#newsletter-form input[name="EMAIL"]').val()
 
         if(email) {
             $.post('https://citizenos.com/.netlify/functions/newsletter', JSON.stringify({ email: email }), function (data) {
                 console.log(data)
 
-                $('#mailchimp').modal('hide')
-                $('#mailchimp-form input[name="EMAIL"]').val('')
+                $('#newsletter').modal('hide')
+                $('#newsletter-form input[name="EMAIL"]').val('')
             }, 'json')
         }
     })
 
-    $('#mailchimp').on('shown.bs.modal', function (e) {
-        $('#mailchimp-form input[name="EMAIL"]').focus()
+    $('#newsletter').on('shown.bs.modal', function (e) {
+        $('#newsletter-form input[name="EMAIL"]').focus()
     })
 
-    $('#mailchimp').on('hide.bs.modal', function (e) {
-        $('#mailchimp-form input[name="EMAIL"]').val('')
+    $('#newsletter').on('hide.bs.modal', function (e) {
+        $('#newsletter-form input[name="EMAIL"]').val('')
     })
 
     var subscribe = function () {
-        $('#mailchimp').modal('show')
+        $('#newsletter').modal('show')
     }
 
     $('a[href="#subscribe"]').click(subscribe)
