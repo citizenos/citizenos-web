4660e1349a65a35930ce224f4ade5b24365f3383
Argo Roots <argo@roots.ee>
2019-09-09 09:39:46 +0300

    Convert Medium XML feed to JSON

--- functions/news.js
+++ functions/news.js
@@ -1,9 +1,9 @@
 const http = require('https')
-
+const xml2js = require('xml2js')
 
 const getMediumJson = async () => {
     return new Promise((resolve, reject) => {
-        http.get('https://medium.com/citizenos/latest?format=json', response => {
+        http.get('https://medium.com/feed/citizenos', response => {
             var body = ''
 
             response.on('data', function(d) {
@@ -11,8 +11,10 @@ const getMediumJson = async () => {
             })
 
             response.on('end', function() {
-                var parsed = JSON.parse(body.replace('])}while(1);</x>', ''))
-                resolve(parsed.payload.posts)
+                xml2js.parseStringPromise(body).then(function (result) {
+                  resolve(result)
+                })
+
             })
         })
   })
@@ -21,15 +23,6 @@ const getMediumJson = async () => {
 
 exports.handler = async (event) => {
     const posts = await getMediumJson()
-    const result = posts.map(p => {
-        return {
-            url: 'https://medium.com/citizenos/' + p.uniqueSlug,
-            title: p.title,
-            text: p.virtuals.subtitle,
-            picture: 'https://miro.medium.com/fit/c/600/300/' + p.virtuals.previewImage.imageId,
-            tags: p.virtuals.tags.map(t => t.slug)
-        }
-    })
 
     const response = {
         statusCode: 200,
@@ -37,7 +30,7 @@ exports.handler = async (event) => {
             'Access-Control-Allow-Origin': '*',
             'Cache-Control': 'max-age=300, must-revalidate'
         },
-        body: JSON.stringify(result)
+        body: JSON.stringify(posts)
     }
 
     return response
