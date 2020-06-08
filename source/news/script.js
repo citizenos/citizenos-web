3eadce95db7e54f44b8e27720ac3b6e9987e9aa4
Argo Roots <argo@roots.ee>
2020-06-08 16:42:08 +0300

    Add news filter

--- source/news/script.js
+++ source/news/script.js
@@ -15,6 +15,7 @@ $(function () {
     $('#all-stories .block-text').addClass('col-sm-10')
     $('#all-stories .block-text').addClass('offset-sm-1')
     $('#all-stories .block-text').append($('#all-stories-blocks').html())
+    $('#all-stories .block-text h2').remove()
     $('#all-stories-blocks').remove()
 
     $('#organizations').removeClass('col-sm-6')
@@ -23,4 +24,21 @@ $(function () {
     $('#organizations .block-text').addClass('col-sm-4')
     $('#organizations .block-text').addClass('offset-sm-4')
     $('#organizations .block-text').addClass('text-center')
+
+
+    $('.cat-link').click(function (e) {
+        e.preventDefault()
+
+        var cat = $(this).data('cat')
+
+        $('.cat-link').removeClass('active')
+        $(this).addClass('active')
+
+        if (cat === 'all') {
+            $('.cat-box').removeClass('d-none')
+        } else {
+            $('.cat-box').addClass('d-none')
+            $('.cat-box[data-cat*="#' + cat + '#"]').removeClass('d-none')
+        }
+    })
 })
