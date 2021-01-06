695d8767eb0310b8b120ae431be56a6a03828094
Argo Roots <argo.roots@adm.ee>
2021-01-06 10:06:02 +0200

    More JS cleanup

--- source/_scripts/navigation.js
+++ source/_scripts/navigation.js
@@ -6,4 +6,6 @@ $(function () {
     $('.select-language').click(function () {
         window.localStorage.setItem('language', $(this).data('language'))
     })
+
+    $('a[href="https://app.citizenos.com"]').attr('href', 'https://app.citizenos.com/#{self.locale}/')
 })
