666e24b8f5fc892368bcc9ccf03d7c21fdac729b
Argo Roots <argo.roots@adm.ee>
2021-01-08 13:46:36 +0200

    App link fix

--- source/_scripts/navigation.js
+++ source/_scripts/navigation.js
@@ -7,5 +7,5 @@ $(function () {
         window.localStorage.setItem('language', $(this).data('language'))
     })
 
-    $('a[href="https://app.citizenos.com"]').attr('href', 'https://app.citizenos.com/#{self.locale}/')
+    $('a[href="https://app.citizenos.com"]').attr('href', 'https://app.citizenos.com/' + $('html').attr('lang') + '/')
 })
