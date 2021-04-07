6e7cb84ee24b1d67ef129d7ac173c464eb7544ce
Liina Guiter <Liina@citizenos.com>
2021-04-07 17:25:07 +0300

    Revert "Deleted a copy of /pyln page"
    This reverts commit 5ef8d7198958b137ed2c1fd725df772c2bde19ba.

---
+++ source/dip/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    //- $('#start-img .block-text').removeClass('col-sm-8')
+    //- $('#start-img .block-text').addClass('col-sm-10')
+    //- $('#start-img .block-text').removeClass('offset-sm-2')
+    //-
+    //- $('#awareness').removeClass('col-sm-6')
+    //- $('#awareness .block-text').removeClass('col-sm-8')
+    //- $('#awareness .block-text').removeClass('offset-sm-2')
+    //- $('#awareness .block-text').addClass('col-sm-4')
+    //- $('#awareness .block-text').addClass('p-5')
+    //- $('#awareness .block-text').addClass('bg-white')
+    //- $('#awareness .block-text').addClass('text-center')
+    //- $('#awareness .block-text').addClass('offset-sm-1')
+    //- $('#awareness .block-text').addClass('box-shadow')
+})
