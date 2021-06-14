5efd56c0c98d5934e9b43625fc1860bb96307c03
Argo Roots <argo@roots.ee>
2021-06-14 12:51:28 +0300

    Remove cookie warning #205

--- source/_scripts/cookie.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    if (window.localStorage.getItem('cookies-accepted') !== 'true') {
-        $('#kypsis').removeClass('d-none')
-    }
-    $('#kypsis button').click(function () {
-        window.localStorage.setItem('cookies-accepted', 'true')
-        $('#kypsis').remove()
-    })
-})
