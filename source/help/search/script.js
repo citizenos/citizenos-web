731e40ced4a6f11853f7e53fa2ccf49ebefa3e83
Argo Roots <argo@roots.ee>
2021-10-26 22:07:47 +0300

    Add search

---
+++ source/help/search/script.js
@@ -0,0 +1,31 @@
+$(function () {
+    var urlSearchParams = new URLSearchParams(window.location.search)
+    var params = Object.fromEntries(urlSearchParams.entries())
+
+    $('#search').on('change input', filter)
+    $('#search').val(params.q)
+
+    filter()
+
+    function filter() {
+        var search = $('#search').val().toLowerCase().split(' ')
+
+        $('li.topic-title').removeClass('topic-title-visible')
+        $('li.topic-title').addClass('d-none')
+
+        $('li.topic-title').each(function () {
+            var link = $(this)
+            var current = search.filter(function (s) {
+                return link.data('search').includes(s)
+            }).length === search.length
+
+            if (current) {
+                link.addClass('topic-title-visible')
+                link.removeClass('d-none')
+            }
+        })
+
+        $('.topic-block').addClass('d-none')
+        $('.topic-title-visible').closest('.topic-block').removeClass('d-none')
+    }
+})
