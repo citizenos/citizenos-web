2c7b3fabdf1441905cd188b66163c329c658342b
Argo Roots <argo@roots.ee>
2022-05-02 09:26:11 +0300

    #287 Check that all fields are filled

--- source/help/request/script.js
+++ source/help/request/script.js
@@ -1,5 +1,9 @@
 $(function () {
     $('#request-submit').click(function () {
+        if(!$('#request-form input[name="email"]').val().trim() || !$('#request-form textarea[name="message"]').val().trim()) {
+            return
+        }
+
         $('#request-form').addClass('d-none')
         $('#request-success').removeClass('d-none')
 
