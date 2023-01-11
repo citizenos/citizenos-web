b2a59ecf6473c6c2f1ebcae47793f85429d9d365
Argo Roots <argo@roots.ee>
2023-01-11 13:24:37 +0200

    #358 Remove RA stats

--- functions/stats.js
+++ functions/stats.js
@@ -37,74 +37,14 @@ const getStatsAPI = async () => {
 }
 
 
-const getStatsERE = async () => {
-    return new Promise((resolve, reject) => {
-        http.get('https://kodanik.elurikkuseerakond.ee/api/stats', response => {
-            var body = ''
-
-            response.on('data', function(d) {
-                body += d
-            })
-
-            response.on('end', function() {
-                try {
-                    var parsed = JSON.parse(body)
-                    resolve(parsed.data)
-                } catch (e) {
-                    console.error(e)
-                    resolve({
-                        topicsCreated: 0,
-                        votesCast: 0,
-                        groupsCreated: 0,
-                        usersCreated: 0
-                    })
-                }
-            })
-        }).on('error', (e) => {
-            console.error(e)
-            resolve({
-                topicsCreated: 0,
-                votesCast: 0,
-                groupsCreated: 0,
-                usersCreated: 0
-            })
-        })
-  })
-}
-
-
-const getStatsRAA = async () => {
-    return new Promise((resolve, reject) => {
-        resolve({
-            topicsCreated: 1012,
-            votesCast: 91000,
-            groupsCreated: 107,
-            usersCreated: 50000
-        })
-    })
-}
-
-
 exports.handler = async (event) => {
-    const apiPromise = getStatsAPI()
-    const erePromise = getStatsERE()
-    const raaPromise = getStatsRAA()
-
-    const api = await apiPromise
-    // const ere = await erePromise
-    const ere = {
-        topicsCreated: 0,
-        votesCast: 0,
-        groupsCreated: 0,
-        usersCreated: 0
-    }
-    const raa = await raaPromise
+    const api = await getStatsAPI()
 
     const result = {
-        topicsCreated: api.topicsCreated + ere.topicsCreated + raa.topicsCreated,
-        votesCast: api.votesCast + ere.votesCast + raa.votesCast,
-        groupsCreated: api.groupsCreated + ere.groupsCreated + raa.groupsCreated,
-        usersCreated: api.usersCreated + ere.usersCreated + raa.usersCreated
+        topicsCreated: api.topicsCreated,
+        votesCast: api.votesCast,
+        groupsCreated: api.groupsCreated,
+        usersCreated: api.usersCreated
     }
 
     const response = {
